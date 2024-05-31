0c9d0a38aae4c98bae3cf5f4cb1d5ca4ed6937d8
Bdmnss <lukabadali123@gmail.com>
2024-05-31 19:00:30 +0400

    adding range slider logic

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const copyIcon = document.querySelector(".copy-icon");
 const copyText = document.querySelector(".copied-txt");
 const generatedPassword = document.querySelector(".generated-password");
 generatedPassword.style.color = "rgba(230, 229, 234, 0.247)";
+const slider = document.querySelector(".slider");
+const output = document.querySelector(".character-length");
+output.innerHTML = slider.value;
 
 copyIcon.addEventListener('click', () => {
     if(generatedPassword.style.color !== "rgba(230, 229, 234, 0.247)"){
         copyText.style.display = "block";
     }
 })
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
