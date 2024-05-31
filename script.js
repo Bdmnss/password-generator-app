0bbbadc81364fd5a8065dac4bfce0c6c0b34cf90
Bdmnss <lukabadali123@gmail.com>
2024-05-31 19:28:47 +0400

    adding checkboxes length logic

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ generatedPassword.style.color = "rgba(230, 229, 234, 0.247)";
 const slider = document.querySelector(".slider");
 const output = document.querySelector(".character-length");
 output.innerHTML = slider.value;
+const checkboxArr = [...document.getElementsByClassName("checkbox")];
+const checkedCheckboxArr = [];
+
 
 copyIcon.addEventListener('click', () => {
     if(generatedPassword.style.color !== "rgba(230, 229, 234, 0.247)"){
@@ -15,3 +18,14 @@ copyIcon.addEventListener('click', () => {
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+checkboxArr.forEach((checkbox) => {
+    checkbox.addEventListener("click", () => {
+        if(checkbox.checked == true) {
+            checkedCheckboxArr.push(checkbox)
+        }
+        if(checkbox.checked == false) {
+            checkedCheckboxArr.splice(checkbox, 1)
+        }
+    })
+})
